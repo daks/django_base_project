4555af1aaaaa125af7691ee357972937e2816819
eric <eric@sietch-tabr.com>
2010-12-30 23:25:16 +0100

    initial django base project

---
+++ local_settings.py
@@ -0,0 +1,27 @@
+from settings import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+#MEDIA_URL = 'http://127.0.0.1:8000/static/'
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': '',                      # Or path to database file if using sqlite3.
+#        'USER': '',                      # Not used with sqlite3.
+#        'PASSWORD': '',                  # Not used with sqlite3.
+#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#    }
+#}
+
+# django-debug-toolbar
+# pip install django-debug-toolbar
+#MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+#INTERNAL_IPS = ('127.0.0.1',)
+#INSTALLED_APPS += ('debug_toolbar',)
+
+# django-command-extensions
+# pip install django-extensions Werkzeug
+#INSTALLED_APPS += ('django_extensions',)
